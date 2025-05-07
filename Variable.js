e2aaccd72579c5b34f22173691ced7e555fa5ca5
Miguel Angel <miguelangelgarb@gmail.com>
2025-05-07 15:10:52 -0500

    Variables JavaScript

---
+++ Variable.js
@@ -0,0 +1,10 @@
+var nombre = "Miguel";
+nombre = "Ángel"; // Se pude cambiar
+
+console.log(nombre);
+
+// Características:
+
+//Se puede re-declarar y re-asignar.
+//No respeta el bloque(solo el scope de función o global).
+//Se usa poco hoy en día para problemas de hoisting y scope confuso.
