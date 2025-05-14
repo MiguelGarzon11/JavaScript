2c19e57b6dba3f490625bdf97740da4cda614d26
Miguel Angel <miguelangelgarb@gmail.com>
2025-05-14 18:58:40 -0500

    Debugging

---
+++ browserDevTools/debuggingIssues.js
@@ -0,0 +1,12 @@
+//  Using Browser DevTools – Introducción
+// Las DevTools son herramientas integradas en navegadores como Chrome, Firefox y Edge. Te permiten:
+
+//     Ver el HTML/CSS/JS del sitio.
+//     Depurar código JavaScript.
+//     Monitorear red, memoria y rendimiento.
+
+// En Chrome, accedes con:
+// Click derecho > Inspeccionar
+// o Ctrl + Shift + I / F12
+
+// Debugging Issues
